e5a5add5898e80157b277e877d27abfc1ed38ad9
iamatharvak <kulkarni.atharva.1403@gmail.com>
2024-12-30 09:26:11 +0530

    signin/signup pages

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require("express");
 const app = express();
 const Port = 8000;
 const path = require("path");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost:27017/blogger")
+  .then((e) => console.log("MongoDb Connected"));
+
+const userRoutes = require("./routes/user");
+
+app.use(express.urlencoded({ extended: false }));
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
@@ -10,7 +19,6 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
+app.use("/user", userRoutes);
+
 app.listen(Port, () => console.log("Connected to Server"));
-
-
-
