0b90421f3c39f9a838e65a98fbf980096678559d
iamatharvak <kulkarni.atharva.1403@gmail.com>
2024-12-31 12:33:35 +0530

    comments started

--- routes/blog.js
+++ routes/blog.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const multer = require("multer");
 const path = require("path");
-const Blog = require("../models/blog")
+const Blog = require("../models/blog");
 
 const router = Router();
 
@@ -22,16 +22,24 @@ router.get("/add-new", (req, res) => {
     user: req.user,
   });
 });
+
+router.get("/:id", async (req, res) => {
+  const blog = await Blog.findById(req.params.id);
+  return res.render('blog',{
+    user: req.user,
+    blog,
+  });
+});
 router.post("/", upload.single("coverImage"), async (req, res) => {
   // console.log(req.body);
   // console.log(req.file);
   // console.log(req.user);
-  const {title,body} = req.body;
+  const { title, body } = req.body;
   const blog = await Blog.create({
     body,
     title,
     createdBy: req.user._id,
-    coverImageURL :`/uploads/${req.file.filename}`,
+    coverImageURL: `/uploads/${req.file.filename}`,
   });
   return res.redirect(`/blog/${blog._id}`);
 });
