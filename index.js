ed506835aa2204e5e33a5180c8731b56a5bf19aa
iamatharvak <kulkarni.atharva.1403@gmail.com>
2024-12-31 11:16:36 +0530

    blog creation

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Port = 8000;
 const path = require("path");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
+const Blog = require("./models/blog");
 
 mongoose
   .connect("mongodb://localhost:27017/blogger")
@@ -16,13 +17,17 @@ const { checkforcookie } = require("./middlewares/authentication");
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(checkforcookie("token"));
+app.use(express.static(path.resolve("./public")));
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  const allBlogs = await Blog.find({}); // "-" means descending
+
   res.render("home", {
     user: req.user,
+    blogs: allBlogs,
   });
 });
 
--- models/blog.js
+++ models/blog.js
@@ -16,7 +16,7 @@ const blogSchema = new Schema(
     },
     createdBy: {
       type: Schema.Types.ObjectId,
-      ref: "user", //copied from createddb users
+      ref: "user", //taken from createddb users
     },
   },
   { timestamps: true }
